1219fb56831e329b6198ed43422aca694a2015c0
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-07-04 22:11:22 +0530

    ✅added flights controller and exports to the index file

---
+++ src/controllers/flight-controller.js
@@ -0,0 +1,69 @@
+const { StatusCodes } = require("http-status-codes");
+const { FlightService } = require("../services");
+const { ApiError, ApiResponse } = require("../utils");
+
+async function createFlight(req, res) {
+  try {
+    console.log("inside controller");
+    const flight = await FlightService.createFlight({
+      flightNumber: req.body.flightNumber,
+      airplaneId: req.body.airplaneId,
+      departureAirportId: req.body.departureAirportId,
+      arrivalAirportId: req.body.arrivalAirportId,
+      arrivalTime: req.body.arrivalTime,
+      departureTime: req.body.departureTime,
+      price: req.body.price,
+      boardingGate: req.body.boardingGate,
+      totalSeats: req.body.totalSeats,
+    });
+
+    return res
+      .status(StatusCodes.CREATED)
+      .json(
+        new ApiResponse(
+          StatusCodes.CREATED,
+          flight,
+          "flight created successfully"
+        )
+      );
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(
+        new ApiError(
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          error,
+          "Unable to create a flight"
+        )
+      );
+  }
+}
+
+async function getAllFlights(req, res) {
+  try {
+    const flights = await FlightService.getAllFlights(req.query);
+    return res
+      .status(StatusCodes.OK)
+      .json(
+        new ApiResponse(
+          StatusCodes.OK,
+          flights,
+          "All flights fetched successfully"
+        )
+      );
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(
+        new ApiError(
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          error,
+          "Unable to fetch all flights"
+        )
+      );
+  }
+}
+module.exports = {
+  createFlight,
+  getAllFlights,
+};
