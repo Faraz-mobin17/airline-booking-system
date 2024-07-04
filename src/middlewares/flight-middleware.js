17beb65edbc070444bb010c2a8a850338afc22f9
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-07-04 22:11:46 +0530

    ✅ added flight middleware and exports in index file

---
+++ src/middlewares/flight-middleware.js
@@ -0,0 +1,81 @@
+const { StatusCodes } = require("http-status-codes");
+const { ApiError } = require("../utils");
+
+function validateFlightRequest(req, res, next) {
+  if (!req.body.flightNumber) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(
+        new ApiError(StatusCodes.BAD_REQUEST, null, "flight number not found")
+      );
+  }
+  if (!req.body.airplaneId) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(
+        new ApiError(StatusCodes.BAD_REQUEST, null, "airplaneId not found")
+      );
+  }
+  if (!req.body.departureAirportId) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(
+        new ApiError(
+          StatusCodes.BAD_REQUEST,
+          null,
+          "departureAirportId not found"
+        )
+      );
+  }
+  if (!req.body.arrivalAirportId) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(
+        new ApiError(
+          StatusCodes.BAD_REQUEST,
+          null,
+          "arrivalAirportId not found"
+        )
+      );
+  }
+  if (!req.body.arrivalTime) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(
+        new ApiError(StatusCodes.BAD_REQUEST, null, "arrivalTime not found")
+      );
+  }
+  if (!req.body.departureTime) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(
+        new ApiError(StatusCodes.BAD_REQUEST, null, "departureTime not found")
+      );
+  }
+  if (!req.body.price) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(new ApiError(StatusCodes.BAD_REQUEST, null, "price not found"));
+  }
+  if (!req.body.boardingGate) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(
+        new ApiError(StatusCodes.BAD_REQUEST, null, "boardingGate not found")
+      );
+  }
+
+  if (!req.body.totalSeats) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(
+        new ApiError(StatusCodes.BAD_REQUEST, null, "totalSeats not found")
+      );
+  }
+
+  next();
+}
+
+module.exports = {
+  validateFlightRequest,
+};
