d041255e837b10a1a5eb89a1991be2cc030ad2f1
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-07-06 20:11:16 +0530

    ✅added query params for  sort filter, travellers and tripdate

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -19,6 +19,8 @@ async function createFlight(data) {
 
 async function getAllFlights(query) {
   let customFilter = {};
+  let sortFilter = [];
+  const ENDING_TRIP_TIME = "23:59:59";
   if (query.trips) {
     [departureAirportId, arrivalAirportId] = query.trips.split("-");
     customFilter.departureAirportId = departureAirportId;
@@ -30,6 +32,22 @@ async function getAllFlights(query) {
     customFilter.price = {
       [Op.between]: [minPrice, maxPrice == undefined ? 20000 : maxPrice],
     };
+  }
+
+  if (query.travellers) {
+    customFilter.totalSeats = {
+      [Op.gte]: query.travellers,
+    };
+  }
+  if (query.tripDate) {
+    customFilter.departureDate = {
+      [Op.between]: [query.tripDate, query.tripDate + ENDING_TRIP_TIME],
+    };
+  }
+  if (query.sort) {
+    const params = query.sort.split(",");
+    const sortFilters = params.map((param) => param.split("_"));
+    sortFilter = sortFilters;
   }
   try {
     const flights = await flightRepository.getAllFlights(customFilter);
