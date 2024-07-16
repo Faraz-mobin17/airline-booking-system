a31f266c3c62d35b9e2bf07a8a217763cab062d9
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-07-16 17:40:42 +0530

    ✅ update remaining seats service layer logic

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -74,8 +74,26 @@ async function getFlight(id) {
   }
 }
 
+async function updateRemainingSeats(data) {
+  try {
+    const response = await flightRepository.updateRemainingSeats(
+      data.flightId,
+      data.seats,
+      data.dec
+    );
+    return response;
+  } catch (error) {
+    throw new ApiError(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      error,
+      "Cannot update the remaining seats of the flight"
+    );
+  }
+}
+
 module.exports = {
   createFlight,
   getAllFlights,
   getFlight,
+  updateRemainingSeats,
 };
